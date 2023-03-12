bd1f38ce9e735f1f036775b92d1bc82738ed0206
pmx <mingl0280@gmail.com>
2023-03-12 16:43:34 -0600

    添加项目文件。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ModifyEnvVars
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.buttonEditText = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonNew
+            // 
+            resources.ApplyResources(this.buttonNew, "buttonNew");
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonEdit
+            // 
+            resources.ApplyResources(this.buttonEdit, "buttonEdit");
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonBrowse
+            // 
+            resources.ApplyResources(this.buttonBrowse, "buttonBrowse");
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonDelete
+            // 
+            resources.ApplyResources(this.buttonDelete, "buttonDelete");
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonMoveUp
+            // 
+            resources.ApplyResources(this.buttonMoveUp, "buttonMoveUp");
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            this.buttonMoveUp.Click += new System.EventHandler(this.buttonMoveUp_Click);
+            // 
+            // buttonMoveDown
+            // 
+            resources.ApplyResources(this.buttonMoveDown, "buttonMoveDown");
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            this.buttonMoveDown.Click += new System.EventHandler(this.buttonMoveDown_Click);
+            // 
+            // buttonEditText
+            // 
+            resources.ApplyResources(this.buttonEditText, "buttonEditText");
+            this.buttonEditText.Name = "buttonEditText";
+            this.buttonEditText.UseVisualStyleBackColor = true;
+            this.buttonEditText.Click += new System.EventHandler(this.buttonEditText_Click);
+            // 
+            // buttonSave
+            // 
+            resources.ApplyResources(this.buttonSave, "buttonSave");
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOK
+            // 
+            resources.ApplyResources(this.buttonOK, "buttonOK");
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonQuit
+            // 
+            resources.ApplyResources(this.buttonQuit, "buttonQuit");
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // dataGridViewMain
+            // 
+            resources.ApplyResources(this.dataGridViewMain, "dataGridViewMain");
+            this.dataGridViewMain.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewMain.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.ColumnHeadersVisible = false;
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.RowHeadersVisible = false;
+            this.dataGridViewMain.RowTemplate.Height = 23;
+            this.dataGridViewMain.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMain_CellEndEdit);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonEditText);
+            this.Controls.Add(this.buttonMoveDown);
+            this.Controls.Add(this.buttonMoveUp);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonNew);
+            this.Name = "FormMain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonMoveUp;
+        private System.Windows.Forms.Button buttonMoveDown;
+        private System.Windows.Forms.Button buttonEditText;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ModifyEnvVars
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            _varsDt.Columns.Add("Values");
+            dataGridViewMain.DataSource = _varsDt;
+            dataGridViewMain.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private List<string> _envValues = new List<string>();
+        private string _envRawStr = "";
+        RegistryKey env_key = null;
+        private DataTable _varsDt = new DataTable("Paths");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                env_key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", true);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Resource.FormMain_Form1_Load_Please_restart_the_app_in_Administrator_privilege_, Resource.FormMain_Form1_Load_Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+
+            if (env_key == null)
+            {
+                MessageBox.Show(Resource.FormMain_Form1_Load_Please_restart_the_app_in_Administrator_privilege_, Resource.FormMain_Form1_Load_Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+            _envRawStr = env_key.GetValue("Path") as string;
+            _envValues = _envRawStr.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            
+            foreach (var env_value in _envValues)
+            {
+                _varsDt.Rows.Add(env_value);
+            }
+
+            dataGridViewMain.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            var idx = dataGridViewMain.CurrentCell.RowIndex;
+
+            var data_row = _varsDt.NewRow();
+            _varsDt.Rows.InsertAt(data_row, idx+1);
+            
+            dataGridViewMain.CurrentCell = dataGridViewMain[0, idx+1];
+
+            dataGridViewMain.BeginEdit(false);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            UpdateVarsList();
+            var results_str = string.Join(";", _envValues);
+            env_key.SetValue("path", results_str, RegistryValueKind.String);
+            MessageBox.Show(Resource.FormMain_buttonSave_Click_Save_complete_, Resource.FormMain_buttonSave_Click_Info, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void dataGridViewMain_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateVarsList();
+        }
+
+        private void UpdateVarsList()
+        {
+            _envValues.Clear();
+            foreach (DataRow vars_dt_row in _varsDt.Rows)
+            {
+                _envValues.Add(vars_dt_row[0] as string);
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            var idx = dataGridViewMain.CurrentCell.RowIndex;
+            dataGridViewMain.CurrentCell = dataGridViewMain[0, idx];
+            dataGridViewMain.BeginEdit(true);
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var current_cell_text = dataGridViewMain.CurrentCell.Value as string;
+            
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (!string.IsNullOrWhiteSpace(current_cell_text))
+            {
+                dialog.InitialDirectory = (new DirectoryInfo(current_cell_text)).FullName;
+            }
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                if (Directory.Exists(dialog.FileName))
+                {
+                    dataGridViewMain.CurrentCell.Value = dialog.FileName;
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            var idx = dataGridViewMain.CurrentCell.RowIndex;
+            _varsDt.Rows.RemoveAt(idx);
+        }
+
+        private void buttonMoveUp_Click(object sender, EventArgs e)
+        {
+            var idx = dataGridViewMain.CurrentCell.RowIndex;
+            if (idx == 0)
+                return;
+            var row_current = _varsDt.Rows[idx].Clone();
+            _varsDt.Rows.RemoveAt(idx);
+            _varsDt.Rows.InsertAt(row_current, idx-1);
+            dataGridViewMain.ClearSelection();
+            dataGridViewMain.CurrentCell = dataGridViewMain.Rows[idx - 1].Cells[0];
+        }
+
+        private void buttonMoveDown_Click(object sender, EventArgs e)
+        {
+            var idx = dataGridViewMain.CurrentCell.RowIndex;
+            if (idx == _varsDt.Rows.Count - 1)
+                return;
+            var row_current = _varsDt.Rows[idx].Clone();
+            _varsDt.Rows.RemoveAt(idx);
+            _varsDt.Rows.InsertAt(row_current, idx + 1);
+            dataGridViewMain.ClearSelection();
+            dataGridViewMain.CurrentCell = dataGridViewMain.Rows[idx + 1].Cells[0];
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            Save();
+            Application.Exit();
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonEditText_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Resource.FormMain_buttonEditText_Click_Not_implemented_, Resource.FormMain_buttonEditText_Click_Warning, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+
+    public static class ExtendedMethods
+    {
+        public static DataRow Clone(this DataRow Row)
+        {
+            var new_row = Row.Table.NewRow();
+            new_row.ItemArray = Row.ItemArray;
+            return new_row;
+        }
+    }
+}
